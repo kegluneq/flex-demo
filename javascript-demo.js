c5b6526858d465348d86ded1aa312b831303f078
Demo User <demouser@MacBook-Pro.local>
2018-07-10 14:17:05 -0700

    Add aria-live instructions to alert screen readers that the button text will be changing

--- javascript-demo.js
+++ javascript-demo.js
@@ -5,6 +5,8 @@
 function addFunButton() {
   // Ask the document to create a button for us.
   let button = document.createElement("button");
+  // Tell screen readers that this button will be changing.
+  button.setAttribute("aria-live", "polite");
   // Set the text inside the button.
   button.innerText = "Let's have fun!";
   // Make the button all fancy
@@ -13,7 +15,7 @@ function addFunButton() {
   button.style.padding = '12px';
 
   let focusHandler = () => {
-    button.innerText = "Are you ready?";
+    button.innerText = "Are you ready to have fun?";
   };
   // Tell the button what to do when we hover the mouse over it or
   // it gains keyboard focus.
@@ -28,8 +30,7 @@ function addFunButton() {
   button.addEventListener("blur", blurHandler);
   // Tell button what to do when we click it.
   button.addEventListener("click", () => {
-    alert("Now we're having fun!");
-    button.innerText = 'We had fun';
+    button.innerText = "We are having fun";
     button.removeEventListener("mouseover", focusHandler);
     button.removeEventListener("focus", focusHandler);
     button.removeEventListener("mouseout", blurHandler);
@@ -37,6 +38,9 @@ function addFunButton() {
     button.setAttribute("disabled", "true");
     button.style.backgroundColor = 'grey';
     button.style.color = 'black';
+    setTimeout(() => {
+      button.innerText = "We had fun!";
+    }, 2500);
   });
   document.body.appendChild(button);
 }
